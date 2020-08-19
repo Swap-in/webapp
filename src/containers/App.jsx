608c9e6d072fc3c6400834e46fdc03e10c1cefd0
Sergio leal <sergioleal@Sergios-MacBook-Pro.local>
2020-08-19 10:23:15 -0500

    [feature] added logout page and routes

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import './App.scss'
 import Login from '../pages/login'
 import Register from '../pages/register'
+import Logout from '../pages/logout'
 
 function App() {
   return (
-    <>
-      <Register/>
+    <> 
+    <Router>
+      <Switch>
+        <Route exact path='/login' component={Login}/>
+        <Route exact path='/registro' component={Register}/>
+        <Route exact path='/' component={Logout}/>
+      </Switch>
+    </Router>
     </>
   )
 }
