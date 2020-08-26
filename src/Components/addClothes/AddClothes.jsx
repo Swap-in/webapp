0c39d58b0de90438d52d98484de30ec06db0c080
Sergio leal <sergioleal@Sergios-MacBook-Pro.local>
2020-08-25 20:11:43 -0500

    [modify] added input fields to upload clothes

--- src/Components/addClothes/AddClothes.jsx
+++ src/Components/addClothes/AddClothes.jsx
@@ -1,10 +1,23 @@
-/* eslint-disable jsx-a11y/label-has-associated-control */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react'
+import React, { useState } from 'react'
 import './AddClothes.scss'
 import Button from '../button/Button'
 
+
 function AddClothes() {
+
+  const [clotheType, setClotheType] = useState('')
+  const [brand, setBrand] = useState('')
+  const [size, setSize] = useState('')
+  const [gender, setGender] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleUpload = (e) => {
+    e.preventDefault();
+    console.log(clotheType, brand, size, gender, description)
+  }
+
+
+
   return (
     <>
       <div className='AddClothes'>
@@ -13,42 +26,67 @@ function AddClothes() {
             <form action=''>
               <label htmlFor=''>
                 Tipo de prenda
-                <select className='AddClothes--form__input'>
-                  <option defaultValue value='' />
-                  <option value='' />
-                  <option value='' />
-                  <option value='' />
+                <select 
+                  className='AddClothes--form__input'
+                  name='clotheType'
+                  onChange={(e) => setClotheType(e.target.value)}
+                >
+                  <option defaultValue value=''/>
+                  <option value='Cabeza'>Cabeza</option>
+                  <option value='Torso'>Torso</option>
+                  <option value='Piernas'>Piernas</option>
+                  <option value='Pies'>Pies</option>
                 </select>
               </label>
               <label htmlFor=''>
                 Marca
-                <input type='text' name='marca' className='AddClothes--form__inputText' required />
+                <input 
+                  type='text'
+                  name='brand' 
+                  className='AddClothes--form__inputText' 
+                  onChange={(e) => setBrand(e.target.value)}
+                  required />
               </label>
               <label htmlFor='type'>
-                Tipo de prenda
-                <select className='AddClothes--form__input' id='type'>
+                Talla
+                <select 
+                  className='AddClothes--form__input' 
+                  name='size' 
+                  onChange={(e) => setSize(e.target.value)}
+                  id='type'
+                >
                   <option defaultValue value='' />
-                  <option value='' />
-                  <option value='' />
-                  <option value='' />
+                  <option value='XS'>XS</option>
+                  <option value='S'>S</option>
+                  <option value='M'>M</option>
+                  <option value='L'>L</option>
+                  <option value='XL'>XL</option>
                 </select>
               </label>
               <label>
-                Tipo de prenda
-                <select className='AddClothes--form__input'>
+                Género
+                <select
+                 className='AddClothes--form__input'
+                 name='gender' 
+                 onChange={(e) => setGender(e.target.value)}
+                >
                   <option defaultValue value='' />
-                  <option value='' />
-                  <option value='' />
-                  <option value='' />
+                  <option value='Hombre'>Hombre</option>
+                  <option value='Mujer'>Mujer</option>
+                  <option value='Unisex'>Unisex</option>
                 </select>
               </label>
               <label htmlFor='description'>
                 Descripción
-                <textarea id='description' />
+                <textarea 
+                  id='description' 
+                  name='description' 
+                  onChange={(e) => setDescription(e.target.value)}/>
               </label>
               <Button
                 title='GUARDAR'
                 className='AddClothes--button'
+                onClick={handleUpload}
               />
             </form>
           </div>
